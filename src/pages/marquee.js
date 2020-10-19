c629365539a362da86a58c11bb61118a00f801bc
compliment <nishkumar1@outlook.com>
2020-10-19 20:05:23 +0530

    Add Notice for MET-2020

---
+++ src/pages/marquee.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Marquee = () => (
+  <>
+  <h4>
+    <marquee><a href="/notice/">Admission open for M.Ed. 2020 </a></marquee>
+  </h4>
+  </>
+)
+export default Marquee
--- src/pages/post-notice.js
+++ src/pages/post-notice.js
@@ -8,6 +8,36 @@ const NoticeBoard = () => (
     <hr></hr>
     <br></br>
     <div>
+      <Alert variant="primary">
+        <Alert.Heading>M.Ed. Admission Notice & Information Brochure (2020)</Alert.Heading>
+        <p>19.10.2020 — M.Ed.</p>
+        <hr />
+        <p className="mb-0">
+          <a href="../documents/MEd/InformationBrochureMET-2020.pdf">
+            <Button variant="outline-dark" size="lg" block>
+              Information Brochure MET-2020
+            </Button>
+          </a>
+        </p><br />
+        <p className="mb-0">
+          <a href="../documents/MEd/AdmissionNoticeMET-2020.pdf">
+            <Button variant="outline-dark" size="lg" block>
+              Admission Notice MET-2020
+            </Button>
+          </a>
+        </p><br />
+        <p className="mb-0">
+          <a href="../documents/MEd/ApplicationFormMET-2020.pdf">
+            <Button variant="outline-dark" size="lg" block>
+              Application Form MET-2020
+            </Button>
+          </a>
+        </p>
+      </Alert>
+    </div>
+    <hr></hr>
+    <br></br>
+    <div>
       <Alert variant="success">
         <Alert.Heading>List of admitted students in B.Ed.</Alert.Heading>
         <p>20.03.2020 — B.Ed.</p>
