a780b1c276f915126214ed6f666fb099798b982c
compliment <nishkumar1@outlook.com>
2020-12-03 09:45:01 +0000

    .

--- src/pages/admit-card.js
+++ src/pages/admit-card.js
@@ -2,7 +2,13 @@ import React from "react"
 import Layout from "../components/layout"
 const AdmitCard = () => (
   <Layout pageInfo={{ pageName: "M. Ed. Admit Card" }}>
-  
+  <form>
+  <label>
+    Form no:
+    <input type="text" name="form-no" />
+  </label>
+  <input type="submit" value="Submit" />
+  </form>
 
   </Layout>
 )
