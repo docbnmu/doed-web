ba72f3876d3446a2a5503a69ad75228e1cdc1543
compliment <nishkumar1@outlook.com>
2020-12-03 09:31:36 +0000

    init admit-card

---
+++ src/pages/admit-card.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Layout from "../components/layout"
+const AdmitCard = () => (
+  <Layout pageInfo={{ pageName: "M. Ed. Admit Card" }}>
+  
+
+  </Layout>
+)
+
+export default AdmitCard
